0a270d6afb32b3cdcc21b3dc8d952d979dd845b3
sekiguchi0731 <108066746+sekiguchi0731@users.noreply.github.com>
2022-12-21 01:41:11 +0900

    Add files via upload

--- sample.js
+++ sample.js
@@ -12,7 +12,7 @@ var a_5 = document.getElementById("a5");
 var counter = 0;
 
 
-function ansa(){
+function ansa() {
     var ans_a = document.getElementById("form_a");
     //値渡し
     //var qbtn2 = document.getElementById("move2").style.visibility;
@@ -25,92 +25,96 @@ function ansa(){
     var a_1 = document.getElementById("a1");
 
     //console.log();
-    if(ans_a.value == "十"){
+    if (ans_a.value == "十") {
         //document.getElementById("move2").style.visibility = "visible";
         //qbtn2.style.visibility = "visible";
         ans_a.disabled = true;
         soshin1.disabled = true;
-        popup.style.display = "block";
-        popup_t.style.display = "block";
-        popup_t2.style.display = "none";
+        // popup.style.display = "block";
+        // popup_t.style.display = "block";
+        // popup_t2.style.display = "none";
+        alertmaker("正解！" + "<br>" + "条件が提示されました！");
         a_1.style.display = "block";
         counter++;
-    }else{
-        popup.style.display = "block";
-        popup_t2.style.display = "block";
+    } else {
+        // popup.style.display = "block";
+        // popup_t2.style.display = "block";
+        alertmaker("不正解..." + "<br>" + "もう一度考えてみよう。")
     }
 }
 
-function displayq1(){
+function displayq1() {
     var q_1 = document.getElementById("q1");
     var q_2 = document.getElementById("q2");
     var q_3 = document.getElementById("q3");
     var q_4 = document.getElementById("q4");
     var q_5 = document.getElementById("q5");
- 
+
     var a_1 = document.getElementById("a1");
     var a_2 = document.getElementById("a2");
     var a_3 = document.getElementById("a3");
     var a_4 = document.getElementById("a4");
     var a_5 = document.getElementById("a5");
 
-    if(q_1.style.display != "block"){
+    if (q_1.style.display != "block") {
         q_1.style.display = "block";
     }
-    if(a_1.style.display != "block" && counter >= 1){
+    if (a_1.style.display != "block" && counter >= 1) {
         a_1.style.display = "block";
     }
-    if(q_2.style.display != "none"){
+    if (q_2.style.display != "none") {
         q_2.style.display = "none";
     }
-    if(a_2.style.display != "none"){
+    if (a_2.style.display != "none") {
         a_2.style.display = "none";
     }
-    if(q_3.style.display != "none"){
+    if (q_3.style.display != "none") {
         q_3.style.display = "none";
     }
-    if(a_3.style.display != "none"){
+    if (a_3.style.display != "none") {
         a_3.style.display = "none";
     }
-    if(q_4.style.display != "none"){
+    if (q_4.style.display != "none") {
         q_4.style.display = "none";
     }
-    if(a_4.style.display != "none"){
+    if (a_4.style.display != "none") {
         a_4.style.display = "none";
     }
-    if(q_5.style.display != "none"){
+    if (q_5.style.display != "none") {
         q_5.style.display = "none";
     }
-    if(a_5.style.display != "none"){
+    if (a_5.style.display != "none") {
         a_5.style.display = "none";
     }
 }
 
-function ansb(){
+function ansb() {
     var ans_b = document.getElementById("form_b");
     var soshin2 = document.getElementById("btn_b");
     //var qbtn3 = document.getElementById("move3");
-    var popup = document.getElementById("ppup-wrap");
-    var popup_t = document.getElementById("ppup");
-    var popup_t2 = document.getElementById("ppup2");
+    // var popup = document.getElementById("ppup-wrap");
+    // var popup_t = document.getElementById("ppup");
+    // var popup_t2 = document.getElementById("ppup2");
     var a_2 = document.getElementById("a2");
 
-    if(ans_b.value == "針"){
+    if (ans_b.value == "針") {
+        alertmaker("正解！" + "<br>" + "条件が提示されました！");
         //qbtn3.style.visibility = "visible";
         ans_b.disabled = true;
         soshin2.disabled = true;
-        popup.style.display = "block";
-        popup_t.style.display = "block";
-        popup_t2.style.display = "none";
+        // popup.style.display = "block";
+        // popup_t.style.display = "block";
+        // popup_t2.style.display = "none";
         a_2.style.display = "block";
         counter++;
-    }else{
-        popup.style.display = "block";
-        popup_t2.style.display = "block";
+    } else {
+        // popup.style.display = "block";
+        // popup_t2.style.display = "block";
+        alertmaker("不正解..." + "<br>" + "もう一度考えてみよう。")
     }
 }
 
-function displayq2(){
+function displayq2() {
     var q_1 = document.getElementById("q1");
     var q_2 = document.getElementById("q2");
     var q_3 = document.getElementById("q3");
@@ -122,62 +126,65 @@ function displayq2(){
     var a_4 = document.getElementById("a4");
     var a_5 = document.getElementById("a5");
 
-    if(q_1.style.display != "none"){
+    if (q_1.style.display != "none") {
         q_1.style.display = "none";
     }
-    if(a_1.style.display != "none"){
+    if (a_1.style.display != "none") {
         a_1.style.display = "none";
     }
-    if(q_2.style.display != "block"){
+    if (q_2.style.display != "block") {
         q_2.style.display = "block";
     }
-    if(a_2.style.display != "block" && counter >= 2){
+    if (a_2.style.display != "block" && counter >= 2) {
         a_2.style.display = "block";
     }
-    if(q_3.style.display != "none"){
+    if (q_3.style.display != "none") {
         q_3.style.display = "none";
     }
-    if(a_3.style.display != "none"){
+    if (a_3.style.display != "none") {
         a_3.style.display = "none";
     }
-    if(q_4.style.display != "none"){
+    if (q_4.style.display != "none") {
         q_4.style.display = "none";
     }
-    if(q_5.style.display != "none"){
+    if (q_5.style.display != "none") {
         q_5.style.display = "none";
     }
-    if(a_4.style.display != "none"){
+    if (a_4.style.display != "none") {
         a_4.style.display = "none";
     }
-    if(a_5.style.display != "none"){
+    if (a_5.style.display != "none") {
         a_5.style.display = "none";
     }
 }
-function ansc(){
+
+function ansc() {
     var ans_c = document.getElementById("form_c");
     var soshin3 = document.getElementById("btn_c");
     //var qbtn4 = document.getElementById("move4");
-    var popup = document.getElementById("ppup-wrap");
-    var popup_t = document.getElementById("ppup");
-    var popup_t2 = document.getElementById("ppup2");
+    // var popup = document.getElementById("ppup-wrap");
+    // var popup_t = document.getElementById("ppup");
+    // var popup_t2 = document.getElementById("ppup2");
     var a_3 = document.getElementById("a3");
 
-    if(ans_c.value == "座"){
+    if (ans_c.value == "座") {
         //qbtn4.style.visibility = "visible";
         ans_c.disabled = true;
         soshin3.disabled = true;
-        popup.style.display = "block";
-        popup_t.style.display = "block";
-        popup_t2.style.display = "none";
+        // popup.style.display = "block";
+        // popup_t.style.display = "block";
+        // popup_t2.style.display = "none";
         a_3.style.display = "block";
         counter++;
-    }else{
-        popup.style.display = "block";
-        popup_t2.style.display = "block";
+        alertmaker("正解！" + "<br>" + "条件が提示されました！");
+    } else {
+        // popup.style.display = "block";
+        // popup_t2.style.display = "block";
+        alertmaker("不正解..." + "<br>" + "もう一度考えてみよう。")
     }
 }
 
-function displayq3(){
+function displayq3() {
     var q_1 = document.getElementById("q1");
     var q_2 = document.getElementById("q2");
     var q_3 = document.getElementById("q3");
@@ -189,64 +196,67 @@ function displayq3(){
     var a_3 = document.getElementById("a3");
     var a_4 = document.getElementById("a4");
     var a_5 = document.getElementById("a5");
-    if(q_1.style.display != "none"){
+    if (q_1.style.display != "none") {
         q_1.style.display = "none";
     }
-    if(q_2.style.display != "none"){
+    if (q_2.style.display != "none") {
         q_2.style.display = "none";
     }
-    if(q_3.style.display != "block"){
+    if (q_3.style.display != "block") {
         q_3.style.display = "block";
     }
-    if(q_4.style.display != "none"){
+    if (q_4.style.display != "none") {
         q_4.style.display = "none";
     }
-    if(q_5.style.display != "none"){
+    if (q_5.style.display != "none") {
         q_5.style.display = "none";
     }
 
-    if(a_1.style.display != "none"){
+    if (a_1.style.display != "none") {
         a_1.style.display = "none";
     }
-    if(a_2.style.display != "none"){
+    if (a_2.style.display != "none") {
         //document.write("で"); ←表示される
         a_2.style.display = "none";
     }
-    if(a_3.style.display != "block" && counter >= 3){
+    if (a_3.style.display != "block" && counter >= 3) {
         a_3.style.display = "block";
     }
-    if(a_4.style.display != "none"){
+    if (a_4.style.display != "none") {
         a_4.style.display = "none";
     }
-    if(a_5.style.display != "none"){
+    if (a_5.style.display != "none") {
         a_5.style.display = "none";
     }
 }
-function ansd(){
+
+function ansd() {
     var ans_d = document.getElementById("form_d");
     var soshin4 = document.getElementById("btn_d");
     //var qbtn5 = document.getElementById("move5");
-    var popup = document.getElementById("ppup-wrap");
-    var popup_t = document.getElementById("ppup");
-    var popup_t2 = document.getElementById("ppup2");
+    // var popup = document.getElementById("ppup-wrap");
+    // var popup_t = document.getElementById("ppup");
+    // var popup_t2 = document.getElementById("ppup2");
     var a_4 = document.getElementById("a4");
 
-    if(ans_d.value == "ぼたん"){
+    if (ans_d.value == "ぼたん") {
         //qbtn5.style.visibility = "visible";
         ans_d.disabled = true;
         soshin4.disabled = true;
-        popup.style.display = "block";
-        popup_t.style.display = "block";
-        popup_t2.style.display = "none";
+        // popup.style.display = "block";
+        // popup_t.style.display = "block";
+        // popup_t2.style.display = "none";
         a_4.style.display = "block";
         counter++;
-    }else{
-        popup.style.display = "block";
-        popup_t2.style.display = "block";
+        alertmaker("正解！" + "<br>" + "条件が提示されました！");
+    } else {
+        // popup.style.display = "block";
+        // popup_t2.style.display = "block";
+        alertmaker("不正解..." + "<br>" + "もう一度考えてみよう。")
     }
 }
 
-function displayq4(){
+function displayq4() {
     var q_1 = document.getElementById("q1");
     var q_2 = document.getElementById("q2");
     var q_3 = document.getElementById("q3");
@@ -258,66 +268,69 @@ function displayq4(){
     var a_4 = document.getElementById("a4");
     var a_5 = document.getElementById("a5");
 
-    if(q_1.style.display != "none"){
+    if (q_1.style.display != "none") {
         q_1.style.display = "none";
     }
-    if(q_2.style.display != "none"){
+    if (q_2.style.display != "none") {
         q_2.style.display = "none";
     }
-    if(q_3.style.display != "none"){
+    if (q_3.style.display != "none") {
         q_3.style.display = "none";
     }
-    if(q_4.style.display != "block"){
+    if (q_4.style.display != "block") {
         q_4.style.display = "block";
     }
-    if(q_5.style.display != "none"){
+    if (q_5.style.display != "none") {
         q_5.style.display = "none";
     }
 
-    if(a_1.style.display != "none"){
+    if (a_1.style.display != "none") {
         a_1.style.display = "none";
     }
-    if(a_2.style.display != "none"){
+    if (a_2.style.display != "none") {
         a_2.style.display = "none";
     }
-    if(a_3.style.display != "none"){
+    if (a_3.style.display != "none") {
         a_3.style.display = "none";
     }
-    if(a_4.style.display != "block" && counter >= 4){
+    if (a_4.style.display != "block" && counter >= 4) {
         a_4.style.display = "block";
     }
-    if(a_5.style.display != "none"){
+    if (a_5.style.display != "none") {
         a_5.style.display = "none";
     }
 }
-function anse(){
+
+function anse() {
     var ans_e = document.getElementById("form_e");
     var soshin5 = document.getElementById("btn_e");
     // var qbtn6 = document.getElementById("move6");
-    var popup = document.getElementById("ppup-wrap");
-    var popup_t = document.getElementById("ppup");
-    var popup_t2 = document.getElementById("ppup2");
-    var popup_last = document.getElementById("ppup3");
+    // var popup = document.getElementById("ppup-wrap");
+    // var popup_t = document.getElementById("ppup");
+    // var popup_t2 = document.getElementById("ppup2");
+    // var popup_last = document.getElementById("ppup3");
     var a_5 = document.getElementById("a5");
 
-    if(ans_e.value == "ten" || ans_e.value == "TEN" || ans_e.value == "Ten"){
+    if (ans_e.value == "ten" || ans_e.value == "TEN" || ans_e.value == "Ten") {
         // qbtn6.style.visibility = "visible";
         ans_e.disabled = true;
         soshin5.disabled = true;
-        popup.style.display = "block";
-        popup_last.style.display = "none";
-        popup_t.style.display = "block";
-        popup_t2.style.display = "none";
+        // popup.style.display = "block";
+        // popup_last.style.display = "none";
+        // popup_t.style.display = "block";
+        // popup_t2.style.display = "none";
         a_5.style.display = "block";
         counter++;
-    }else{
-        popup_last.style.display = "none";
-        popup.style.display = "block";
-        popup_t2.style.display = "block";
+        alertmaker("正解！" + "<br>" + "条件が提示されました！");
+    } else {
+        // popup_last.style.display = "none";
+        // popup.style.display = "block";
+        // popup_t2.style.display = "block";
+        alertmaker("不正解..." + "<br>" + "もう一度考えてみよう。")
     }
 }
 
-function displayq5(){
+function displayq5() {
     var q_1 = document.getElementById("q1");
     var q_2 = document.getElementById("q2");
     var q_3 = document.getElementById("q3");
@@ -331,35 +344,35 @@ function displayq5(){
     var popup = document.getElementById("ppup-wrap");
     var popup_last = document.getElementById("ppup3");
 
-    if(q_1.style.display != "none"){
+    if (q_1.style.display != "none") {
         q_1.style.display = "none";
     }
-    if(q_2.style.display != "none"){
+    if (q_2.style.display != "none") {
         q_2.style.display = "none";
     }
-    if(q_3.style.display != "none"){
+    if (q_3.style.display != "none") {
         q_3.style.display = "none";
     }
-    if(q_4.style.display != "none"){
+    if (q_4.style.display != "none") {
         q_4.style.display = "none";
     }
-    if(q_5.style.display != "block"){
+    if (q_5.style.display != "block") {
         q_5.style.display = "block";
     }
 
-    if(a_1.style.display != "none"){
+    if (a_1.style.display != "none") {
         a_1.style.display = "none";
     }
-    if(a_2.style.display != "none"){
+    if (a_2.style.display != "none") {
         a_2.style.display = "none";
     }
-    if(a_3.style.display != "none"){
+    if (a_3.style.display != "none") {
         a_3.style.display = "none";
     }
-    if(a_4.style.display != "none"){
+    if (a_4.style.display != "none") {
         a_4.style.display = "none";
     }
-    if(a_5.style.display != "block" && counter >= 5){
+    if (a_5.style.display != "block" && counter >= 5) {
         a_5.style.display = "block";
     }
 
@@ -367,23 +380,21 @@ function displayq5(){
     popup_last.style.display = "block";
 }
 
-function popupclose(){
-    var pp = document.getElementById("ppup-wrap");
-    var p1 = document.getElementById("ppup1");
-    var p2 = document.getElementById("ppup2");
-    var p3 = document.getElementById("ppup3");
-    var p4 = document.getElementById("ppup4");
-    var p5 = document.getElementById("ppup5");
+// function popupclose() {
+//     var pp = document.getElementById("ppup-wrap");
+//     var p1 = document.getElementById("ppup");
+//     var p2 = document.getElementById("ppup2");
+//     var p3 = document.getElementById("ppup3");
+//     var p5 = document.getElementById("ppup5");
 
-    pp.style.display = "none";
-    p1.style.display = "none";
-    p2.style.display = "none";
-    p3.style.display = "none";
-    p4.style.display = "none";
-    p5.style.display = "none";
-}
+//     pp.style.display = "none";
+//     p1.style.display = "none";
+//     p2.style.display = "none";
+//     p3.style.display = "none";
+//     p5.style.display = "none";
+// }
 
-function popupclose2(){
+function popupclose2() {
     var pp = document.getElementById("ppup-wrap");
     var p1 = document.getElementById("ppup4");
     var qbtn2 = document.getElementById("move2");
@@ -391,31 +402,64 @@ function popupclose2(){
     var qbtn4 = document.getElementById("move4");
     var qbtn5 = document.getElementById("move5");
 
-    p1.style.display = "none";
-    pp.style.display = "none";
-    if(counter == 1){
+    // p1.style.display = "none";
+    // pp.style.display = "none";
+    if (counter == 1) {
         qbtn2.style.visibility = "visible";
         displayq2();
-    }else if(counter == 2){
+    } else if (counter == 2) {
         qbtn3.style.visibility = "visible";
         displayq3();
-    }else if(counter == 3){
+    } else if (counter == 3) {
         qbtn4.style.visibility = "visible";
         displayq4();
-    }else if(counter == 4){
+    } else if (counter == 4) {
         qbtn5.style.visibility = "visible";
         displayq5();
     }
 }
 
-function next(){
-    var pp = document.getElementById("ppup-wrap");
-    var p1 = document.getElementById("ppup4");
-    var p2 = document.getElementById("ppup5");
+function next() {
+    // var pp = document.getElementById("ppup-wrap");
+    // var p1 = document.getElementById("ppup4");
+    // var p2 = document.getElementById("ppup5");
+    // pp.style.display = "block";
+    // p1.style.display = "block";
 
-    pp.style.display = "block";
-    p1.style.display = "block";
-    if(counter == 5){
-        p2.style.display = "block";
+    if (counter == 1) {
+        $('#btn_ja').prop('disabled', true);
     }
+    if (counter == 2) {
+        $('#btn_jb').prop('disabled', true);
+    }
+    if (counter == 3) {
+        $('#btn_jc').prop('disabled', true);
+    }
+    if (counter == 4) {
+        // alertmaker("次の謎が表示されました！", popupclose2())
+        $('#btn_jd').prop('disabled', true);
+        alertmaker("次の謎が表示されました！" + "<br>" + "これが最後の謎です")
+    }
+    if (counter == 5) {
+        $('#btn_je').prop('disabled', true);
+        alertmaker("必要なものを持って" + "<br>" + "ブースに向かいましょう！")
+    } else {
+        alertmaker("次の謎が表示されました！", popupclose2())
+    }
+}
+
+function alertmaker(popup, callback) {
+    $('#close, #layer, #closetitle').off(); //1回クリックイベントをリセット
+    $(".popuptext").html(popup);
+    $('#popup, #layer').css("display", "block");
+
+    $('#close, #layer, #closetitle').click(function(e) {
+        $('#popup, #layer').hide();
+        if (callback) { //2変数目があれば実行する
+            callback();
+        }
+    });
+    // if (arguments.length == 2) {
+    //     $("#close, #layer").one("click", callback); //one()は誤字じゃないです！
+    // }
 }
